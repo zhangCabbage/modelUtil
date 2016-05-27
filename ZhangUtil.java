8a33e1e63436cae132d1b7ef561695eb21bd4363
jiahua <1094006351@qq.com>
2016-05-27 21:43:59 +0800

    add

--- ZhangUtil.java
+++ ZhangUtil.java
@@ -1,9 +1,19 @@
 package zhang.algorithm.modelUtil;
 
+import java.util.Arrays;
+
 /**
  * Created by zhang_zack on 16/5/26.
  */
 public class ZhangUtil {
+    /**
+     * 打印数组
+     * @param array
+     */
+    public static void printArray(int[] array){
+        System.out.println(Arrays.toString(array));
+    }
+
     /**
      * 输出整数型矩阵
      * @param matrix
@@ -17,5 +27,18 @@ public class ZhangUtil {
             System.out.println();
         }
         System.out.println("-----matrix end-----");
+    }
+
+    /**
+     * 交换数组中两个数
+     */
+    public static void swap(int[] nums, int m, int n){
+        int temp = nums[m];
+        nums[m] = nums[n];
+        nums[n] = temp;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
