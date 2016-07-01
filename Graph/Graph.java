321b72e6c711599fb186a2ab56ce3fc74c44e568
zhang_jiahua <1094006351@qq.com>
2016-07-01 22:33:54 +0800

    add graph

---
+++ Graph/Graph.java
@@ -0,0 +1,14 @@
+package zhang.algorithm.modelUtil.Graph;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: zhang_Lenovo
+ * Date: 2016/7/1
+ * Time: 22:29
+ * To change this template use File | Settings | File Templates.
+ */
+public class Graph {
+
+    //about graph`s breadth first traversal and Depth first traversal
+
+}
