cb8101b512dd772cbf581520e0e85fc5e7d82a7f
jiahua <1094006351@qq.com>
2016-11-09 21:49:27 +0800

    add search trie

---
+++ Search/Trie.java
@@ -0,0 +1,19 @@
+package zhang.algorithm.modelUtil.Search;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jiahua_MacPro
+ * Date: 16/11/9
+ * Time: 下午9:28
+ * To change this template use File | Settings | File Templates.
+ * <p>
+ * 单词查找树的实现, 实现完, 可以看这个类(MaxXORTwoNumArray)
+ * TODO
+ * Trie树，又称字典树，单词查找树或者前缀树(http://dongxicheng.org/structure/trietree/)
+ */
+public class Trie {
+
+    public static void main(String[] args) {
+        Trie test = new Trie();
+    }
+}
