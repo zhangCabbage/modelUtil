b8814202eb23eb3095043ed05fecbebe6e44113d
jiahua <1094006351@qq.com>
2016-11-10 22:41:52 +0800

    add code dp LCS

---
+++ AlgorithmDesign/DynamicProgramming/LCS.java
@@ -0,0 +1,23 @@
+package zhang.algorithm.modelUtil.AlgorithmDesign.DynamicProgramming;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jiahua_MacPro
+ * Date: 16/11/10
+ * Time: 下午10:33
+ * To change this template use File | Settings | File Templates.
+ * <p>
+ * Longest Common Subsequence
+ * eg:
+ * s1: 2 5 7 9 3 1 2
+ * s2: 3 5 3 2 8
+ *
+ * then the LCS is 532
+ */
+public class LCS {
+
+
+    public static void main(String[] args) {
+        LCS test = new LCS();
+    }
+}
