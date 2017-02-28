d1527f2d9afe7a369a5a9b15ddeef3106257d2be
jiahua <1094006351@qq.com>
2017-02-28 21:22:25 +0800

    update sort

--- sort/InsertSort.java
+++ sort/InsertSort.java
@@ -8,10 +8,12 @@ import zhang.algorithm.modelUtil.Array.ArrayTool;
  * Date: 2016/6/23
  * Time: 15:52
  * To change this template use File | Settings | File Templates.
- *
+ * <p>
  * 时间复杂度分析：
  * 最好：比较 n-1 次，O(n)
  * 最坏：比较 n(n-1)/2 次，O(n^2)
+ *
+ * Review Time: 2017-02-28 15:40:25
  */
 public class InsertSort {
     /**
@@ -19,16 +21,17 @@ public class InsertSort {
      *
      * @param nums
      */
-    public static void insertSort(int[] nums){
-        for(int i=1; i<nums.length; i++){
+    public static void insertSort(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
             int k = i - 1;
             int temp = nums[i];
-            while(k >= 0 && temp < nums[k]){
-                nums[k+1] = nums[k--];
+            while (k >= 0 && temp < nums[k]) {
+                nums[k + 1] = nums[k--];
             }
             nums[++k] = temp;
         }
     }
+
 
     /**
      * 插入排序的带哨兵的实现方式
@@ -37,20 +40,20 @@ public class InsertSort {
      * 2、防止越界。
      * 如果增加哨兵，那么每次循环，程序就只有一次判断，提高了近一半的效率。
      * 注意增加哨兵并不是雕虫小技，要深入理解这种技巧！特别是对于使用频率高，记录数较大时，作用就更加显著
-     *
+     * <p>
      * 但是这种方法在使用时，因为传入数据并没有空余下标为0的数组位置，所以存在问题！！
      *
      * @param nums
      */
-    public static void insertSort2(int[] nums){
-        int[] copyNums = new int[nums.length+1];
+    public static void insertSort2(int[] nums) {
+        int[] copyNums = new int[nums.length + 1];
         System.arraycopy(nums, 0, copyNums, 1, nums.length);
 
-        for(int i=2; i<copyNums.length; i++){
+        for (int i = 2; i < copyNums.length; i++) {
             int k = i - 1;
             copyNums[0] = copyNums[i];
-            while(copyNums[0] < copyNums[k]){
-                copyNums[k+1] = copyNums[k--];
+            while (copyNums[0] < copyNums[k]) {
+                copyNums[k + 1] = copyNums[k--];
             }
             copyNums[++k] = copyNums[0];
         }
--- sort/MergeSort.java
+++ sort/MergeSort.java
@@ -4,65 +4,84 @@ import java.util.Arrays;
 
 /**
  * 归并排序的实现
- *
- *
+ * <p>
+ * <p>
  * 时间复杂度分析(采用递推公式的方法进行分析)：
  * T(n) = 2T(n-1) + 2^(n-1)
- *
+ * <p>
  * 时间复杂度分析：
  * 最好：O(nlogn)
  * 最坏：O(nlogn)
  * 平均：O(nlogn)
  *
- * @author zhang_zack
+ * Review Time: 2017-02-28 17:05:12
  *
+ * @author zhang_zack
  */
 public class MergeSort {
-	/**
-	 * 方法一：具体采用递归的方式实现归并排序
-	 * @param corArr
-	 * @param arr
-	 * @param start
-	 * @param end
-	 */
-	private static void mergeRecursive(int[] corArr, int[] arr, int start, int end) {
-		if(start>=end){
-			return;
-		}
-		int mid = start+(end-start)/2;
-		int start1 = start;
-		int end1 = mid;
-		int start2 = mid+1;
-		int end2 = end;
-		mergeRecursive(corArr, arr, start1, end1);
-		mergeRecursive(corArr, arr, start2, end2);
-		
-		//然后合并前后两个半数组
-		int k = start;
-		while(start1<=end1 && start2<=end2){
-			corArr[k++] = (arr[start1]<arr[start2])?arr[start1++]:arr[start2++];
-		}
-		while(start1<=end1){
-			corArr[k++] = arr[start1++];
-		}
-		while(start2<=end2){
-			corArr[k++] = arr[start2++];
-		}
-		
-		//最后把合并后的数组复制给原数组
-		for(int i=start; i<=end; i++){
-			arr[i] = corArr[i];
-		}
-	}
-	
-	public static void mergeSort(int[] arr){
-		int[] corArr = new int[arr.length];
-		mergeRecursive(corArr, arr, 0, arr.length-1);
-	}
+    /**
+     * 方法一：采用递归的方式实现归并排序
+     *
+     * @param corArr
+     * @param arr
+     * @param start
+     * @param end
+     */
+    private static void mergeRecursive(int[] corArr, int[] arr, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+        int mid = start + (end - start) / 2;
+        int start1 = start;
+        int end1 = mid;
+        int start2 = mid + 1;
+        int end2 = end;
+        mergeRecursive(corArr, arr, start1, end1);
+        mergeRecursive(corArr, arr, start2, end2);
 
-	public static void main(String[] args){
-		int[] arr = {3, 5, 4, 1, 8, 6, 9};
-		MergeSort.mergeSort(arr);
-		System.out.println(Arrays.toString(arr));
-	}
+        //然后合并前后两个半数组
+        int k = start;
+        while (start1 <= end1 && start2 <= end2) {
+            corArr[k++] = (arr[start1] < arr[start2]) ? arr[start1++] : arr[start2++];
+        }
+        while (start1 <= end1) {
+            corArr[k++] = arr[start1++];
+        }
+        while (start2 <= end2) {
+            corArr[k++] = arr[start2++];
+        }
+
+        //最后把合并后的数组复制给原数组
+        for (int i = start; i <= end; i++) {
+            arr[i] = corArr[i];
+        }
+
+    }
+
+    public static void mergeSort(int[] arr) {
+        mergeSort(arr, 0, arr.length - 1);
+    }
+
+    private static void mergeSort(int[] array, int start, int end) {
+        if (start < end) {
+            int mid = start + (end - start >> 1);
+            mergeSort(array, start, mid);
+            mergeSort(array, mid + 1, end);
+
+            int[] res = new int[end - start + 1];
+            int i = start, j = mid + 1, k = 0;
+            while (i <= mid && j <= end)
+                res[k++] = (array[i] < array[j]) ? array[i++] : array[j++];
+            while (i <= mid) res[k++] = array[i++];
+            while (j <= end) res[k++] = array[j++];
+
+            System.arraycopy(res, 0, array, start, res.length);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 4, 1, 8, 6, 9};
+        MergeSort.mergeSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 }
